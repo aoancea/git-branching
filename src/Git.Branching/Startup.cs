1ad2f3faf722b6475380d19c1178e88c5ce8ce2c
aoancea <alx.oancea@gmail.com>
2017-01-05 16:52:38 +0200

    Initial project structure

---
+++ src/Git.Branching/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git.Branching.Startup))]
+namespace Git.Branching
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
